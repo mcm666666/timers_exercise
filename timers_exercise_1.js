bccac84c82e559789e41e9595d9a4b76fd29223c
mcm666666 <41454252+mcm666666@users.noreply.github.com>
2022-01-08 16:47:35 -0800

    Add files via upload

---
+++ timers_exercise_1.js
@@ -0,0 +1,27 @@
+function countDown(num){
+    let timer = setInterval(function() {
+      num--;
+      if(num <= 0){
+        clearInterval(timer);
+        console.log('DONE!');
+      }
+      else {
+        console.log(num);
+      }
+  
+    },1000)
+  }
+
+
+function randomGame() {
+    let num;
+    let times = 0;
+    let timer = setInterval(function() {
+        num = Math.random();
+        times++
+        if(num > .75) {
+            clearInterval(timer);
+            console.log("It took " + times + " try/tries.");
+        }
+    }, 1000)
+}
